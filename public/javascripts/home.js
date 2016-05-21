24e6f5495614844715af884eba988ddbc4777036
Wendy <wendygoodwin@mac.com>
2016-05-20 18:07:50 -0700

    slider and basiclayout

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -16,24 +16,7 @@ $(document).ready(function(){
     $('#choirBack').toggle();
     $('#choirVidButton').toggle();
   });
-//
-//
-//   $('.multiple-items').slick({
-//   infinite: true,
-//   slidesToShow: 3,
-//   slidesToScroll: 3,
-//   arrows: true,
-//   autoplay: true,
-//   autoplaySpeed: 5000,
-// });
 
-// $('.multiple-items').slick({
-// dots: true,
-// infinite: true,
-// speed: 500,
-// fade: true,
-// cssEase: 'linear'
-// });
 $('.multiple-items').slick({
   dots: true,
   infinite: true,
@@ -43,11 +26,5 @@ $('.multiple-items').slick({
   autoplaySpeed: 4500,
   cssEase: 'linear'
 });
-
-
-  //
-  // $('.flexslider').flexslider({
-  //   animation: "slide"
-  // });
 
 });
